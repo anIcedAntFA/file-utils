46f9a4663417e853facdca70f58503198577b2fb
ngockhoi96 <anicedfaant@gmail.com>
2025-03-10 00:15:58 +0700

    :wrench: configs(release): update semantic release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,9 +3,11 @@
 # [v1.0.1](https://github.com/anIcedAntFA/file-utils/compare/v1.0.0...v1.0.1) (2025-03-09)
 
 ## 🚑 Critical Hotfixes
+
 - [`9edff2e`](https://github.com/anIcedAntFA/file-utils/commit/9edff2e) ️ Update README file
 
 # v1.0.0 (2025-03-09)
 
 ## ✨ New Features
-- [`6bb8f9b`](https://github.com/anIcedAntFA/file-utils/commit/6bb8f9b)  Add a new feature
+
+- [`6bb8f9b`](https://github.com/anIcedAntFA/file-utils/commit/6bb8f9b) Add a new feature
--- release.config.mjs
+++ release.config.mjs
@@ -1,6 +1,20 @@
-/**
- * @type {import('semantic-release').GlobalConfig}
- */
+import { readFile } from "node:fs";
+import { promisify } from "node:util";
+
+const readFileAsync = promisify(readFile);
+
+// the *.hbs template and partials should be passed as strings of contents
+const template = readFileAsync("./default-template.hbs");
+let choreMessage = "";
+if (process.env.GITHUB_ACTIONS) {
+	// CI IS SET
+	choreMessage = ":construction_worker: chore(release): ${nextRelease.version}";
+} else {
+	// CI IS NOT SET
+	choreMessage =
+		":construction_worker: chore(release): ${nextRelease.version} [skip ci]";
+}
+
 export default {
 	branches: ["main", { name: "next", prerelease: true }],
 	plugins: [
@@ -11,33 +25,58 @@ export default {
 					// Major version bumps
 					major: [
 						":boom:", // "💥": Breaking changes
-						":firecracker:", // "🧨": Breaking changes
+						":firecracker:", // "🧨": Breaking changes,
+						":building_construction:", // "🏗️": Architectural changes.
 					],
 					// Minor version bumps
 					minor: [
 						":sparkles:", // "✨": New feature
-						":tada:", // "🎉": New feature
-						":rocket:", // "🚀": Performance improvements
-						":wheelchair:", // "♿": Accessibility
-						":building_construction:", // "🏗️": Architecture changes
 					],
 					// Patch version bumps
 					patch: [
-						":bug:", // "🐛": Bug fix
-						":adhesive_bandage:", // "🩹": Simple fix
-						":lock:", // "🔒": Security
-						":zap:", // "⚡": Performance
-						":recycle:", // "♻️": Refactor
-						":wrench:", // "🔧": Configuration
-						":arrow_up:", // "⬆️": Dependencies
-						":memo:", // "📝": Documentation
-						":ambulance:", // "🚑": Critical hotfix
-						":magnifying_glass:", // "🔍": SEO improvements
+						":zap:", // "⚡": Improve performance.
+						":bug:", // "🐛": Fix a bug.
+						":ambulance:", // "🚑": Critical hotfix.
+						":lipstick:", // "💄": Add or update the UI and style files.
+						":lock:", // "🔒": Fix security or privacy issues.
+						":arrow_down:", // "⬇️": Downgrade dependencies.
+						":arrow_up:", // "⬆️": Upgrade dependencies.
+						":pushpin:", // "📌": Pin dependencies to specific versions.
+						":chart_with_upwards_trend:", // "📊": Add or update analytics or track code.
+						":heavy_plus_sign:", // "➕": Add a dependency.
+						":heavy_minus_sign:", // "➖": Remove a dependency.
+						":wrench:", // "🔧": Add or update configuration files.
+						":globe_with_meridians:", // "🌐": Internationalization and localization.
+						":pencil2:", // "✏️": Fix typos.
+						":rewind:", // "⏪": Revert changes.
+						":package:", // "📦": Add or update compiled files or packages.
+						":alien:", // "👽": Update code due to external API changes.
+						":bento:", // "🍱": Add or update assets.
+						":wheelchair:", // "♿": Improve accessibility.
+						":speech_balloon:", // "💬": Add or update text and literals.
+						":card_file_box:", // "🗃️": Perform database related changes.
+						":children_crossing:", // "🚸": Improve user experience / usability.
+						":iphone:", // "📱": Work on responsive design.
+						":egg:", // "🥚": Add or update an easter egg.
+						":alembic:", // "🧪": Perform experiments.
+						":mag:", // "🔍": Improve SEO.
+						":label:", // "🏷️": Add or update types.
+						":triangular_flag_on_post:", // "🚩": Add, update, or remove feature flags.
+						":goal_net:", // "🥅": Catch errors.
+						":dizzy:", // "💫": Add or update animations and transitions.
+						":wastebasket:", // "🗑️": Deprecate code that needs to be cleaned up.
+						":passport_control:", // "🛂": Work on code related to authorization, roles, and permissions.
+						":adhesive_bandage:", // "🩹": Simple fix for a non-critical issue.
+						":necktie:", // "👔": Add or update business logic.
+						":recycle:", // "♻️": Refactor code.
 					],
 				},
 				// Ensure `cz-git` and `semantic-release-gitmoji` work correctly together
 				parserOpts: {
 					noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+				},
+				releaseNotes: {
+					template,
 				},
 			},
 		],
