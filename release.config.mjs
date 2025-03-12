00bf6d16a441ac99b533a2d1b5af03abf7b2f2b3
ngockhoi96 <anicedfaant@gmail.com>
2025-03-12 16:40:08 +0700

    :wrench: configs(configs): update config

--- release.config.mjs
+++ release.config.mjs
@@ -99,8 +99,11 @@ export default {
 			"@semantic-release/git",
 			{
 				assets: ["package.json", "CHANGELOG.md"],
-				message:
+				message: [
+					":bookmark: v${nextRelease.version} [skip ci]",
+					"",
 					"chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+				].join("\n"),
 			},
 		],
 		[
